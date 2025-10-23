7a974050540571b56cec2ea5b939697e3346b4dd
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 12:22:10 +0700

    menambahkan logo umy dan memperbaiki bagian collor

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -1,6 +1,8 @@
 package com.example.advancelayout
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,10 +14,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+private val Unit.DarkGray: Color
+
 
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
@@ -38,7 +45,12 @@ fun ActivitasPertama(modifier: Modifier) {
         Card(modifier = Modifier
             .fillMaxWidth(fraction = 1f)
             .padding(all = 12.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = color.DarkGrey
-        ))}
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+        )){
+        Row(){
+            val gambar = painterResource(id = R.drawable.umy_logo)
+            Image(
+            painter = gambar,
+        }
 
