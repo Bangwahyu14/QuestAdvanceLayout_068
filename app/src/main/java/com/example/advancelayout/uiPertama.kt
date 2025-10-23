22fb57076ba3d7e30db2cb74f4f8fb589e7a6757
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 13:12:16 +0700

    memperbaiki coloumb,box dan warna teks pada alamat

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -30,8 +30,7 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column(
-        modifier = Modifier
-            .padding(top = 100.dp)
+        modifier = Modifier.padding(top = 100.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -40,55 +39,58 @@ fun ActivitasPertama(modifier: Modifier) {
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
-    }
-    Text(
-        text = stringResource(id = R.string.univ),
-        fontSize = 22.sp
-    )
-    Spacer(modifier = Modifier.height(25.dp))
-    Card(
-        modifier = Modifier
-            .fillMaxWidth(fraction = 1f)
-            .padding(all = 12.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.DarkGray
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 22.sp
         )
-    ) {
-        Row() {
-            val gambar = painterResource(id = R.drawable.umy_logo)
-            Image(
-                painter = gambar,
-                contentDescription = null,
-                modifier = Modifier.size(100.dp).padding(all = 5.dp)
+        Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
             )
-            Spacer(modifier = Modifier.width(30.dp))
-            Column() {
-                Text(
-                    text = stringResource(id = R.string.nama),
-                    fontSize = 30.sp,
-                    fontFamily = FontFamily.Cursive,
-                    color = Color.White,
-                    modifier = Modifier.padding(top = 15.dp)
+        ) {
+            Row() {
+                val gambar = painterResource(id = R.drawable.umy_logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp).padding(all = 5.dp)
                 )
-                Text(
-                    text = stringResource(id = R.string.alamat),
-                    fontSize = 20.sp,
-                )
-            }
+                Spacer(modifier = Modifier.width(30.dp))
+                Column() {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
 
+                    )
+                }
+
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
         }
     }
-
-Box(
-    modifier = Modifier
-        .fillMaxWidth()
-) {
-    Text(
-        text = stringResource(id = R.string.copy),
-        modifier = Modifier
-            .align(Alignment.BottomCenter)
-            .padding(bottom = 50.dp)
-    )
-}
 }
 
