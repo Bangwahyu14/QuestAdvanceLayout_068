7472058f14e8d7f29c835e03efa0373fed765a0d
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 12:00:48 +0700

    menambahkan spacer untuk jarak elemen

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -1,8 +1,11 @@
 package com.example.advancelayout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -28,4 +31,6 @@ fun ActivitasPertama(modifier: Modifier) {
     }
         Text(
             text = stringResource(id = R.string.univ),
-            fontSize = 22.sp
+            fontSize = 22.sp)
+        Spacer(modifier = Modifier.height(25.dp))
+
