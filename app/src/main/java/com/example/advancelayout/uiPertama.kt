56471f62eae4c29391976b69e5e26f2c1ea5be12
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 11:55:39 +0700

    mendambahkan  code activitas pertama (modifier)

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -3,10 +3,12 @@ package com.example.advancelayout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column(
         modifier = Modifier
