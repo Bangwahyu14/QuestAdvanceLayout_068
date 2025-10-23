c4a4e1bdcd370ba3d1ebe3ee392433d84130abbc
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 12:05:30 +0700

    menambahkan card dengan layout dan warna

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -3,9 +3,11 @@ package com.example.advancelayout
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,4 +35,8 @@ fun ActivitasPertama(modifier: Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp)
         Spacer(modifier = Modifier.height(25.dp))
+        Card(modifier = Modifier
+            .fillMaxWidth(fraction = 1f)
+            .padding(all = 12.dp),
+        colors = CardDefaults.cardColors(
 
