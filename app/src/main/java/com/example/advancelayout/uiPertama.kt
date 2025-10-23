6ffad0b9c18d2a5b2f2009e0534a63fb645445ed
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 11:52:57 +0700

    mendambahkan  code activitas pertama

---
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -0,0 +1,16 @@
+package com.example.advancelayout
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+fun ActivitasPertama(modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
