6bec327f3d51db58af658e13d7b48680793f7026
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-23 12:26:53 +0700

    mengatur ukuran dan padding pada gambar

--- app/src/main/java/com/example/advancelayout/uiPertama.kt
+++ app/src/main/java/com/example/advancelayout/uiPertama.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -51,6 +53,10 @@ fun ActivitasPertama(modifier: Modifier) {
         Row(){
             val gambar = painterResource(id = R.drawable.umy_logo)
             Image(
-            painter = gambar,
+                painter = gambar,
+                modifier = Modifier.size(100.dp).padding(all = 5.dp)
+            )
+
+
         }
 
