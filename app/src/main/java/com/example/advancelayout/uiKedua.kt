9aad47c57ce1bf60646fd3582252b31bcf0915ca
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-24 14:28:23 +0700

    Menambahkan Row untuk menampung logo dan teks dalam Card kedua

--- app/src/main/java/com/example/advancelayout/uiKedua.kt
+++ app/src/main/java/com/example/advancelayout/uiKedua.kt
@@ -1,6 +1,7 @@
 package com.example.advancelayout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -45,6 +46,8 @@ fun AktivitasKedua(modifier: Modifier) {
                     containerColor = Color(DarkBlue)
                     )
         ) {
+            Row {
+            }
         }
     }
 }
