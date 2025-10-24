a6d82940c331ade04b1edecc3653757a7b5f54bd
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-24 14:23:22 +0700

    Menambahkan jarak vertikal menggunakan Spacer di AktivitasKedua

--- app/src/main/java/com/example/advancelayout/uiKedua.kt
+++ app/src/main/java/com/example/advancelayout/uiKedua.kt
@@ -1,7 +1,9 @@
 package com.example.advancelayout
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,5 +31,6 @@ fun AktivitasKedua(modifier: Modifier) {
             text = stringResource(id = R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = Modifier.height(25.dp))
     }
 }
