b515b02dfb6bb4b7787f922dc11c45bf9a48d55b
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-24 14:29:11 +0700

    Menambahkan variabel painterResource untuk gambar logo UMY

--- app/src/main/java/com/example/advancelayout/uiKedua.kt
+++ app/src/main/java/com/example/advancelayout/uiKedua.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,7 +47,7 @@ fun AktivitasKedua(modifier: Modifier) {
                     containerColor = Color(DarkBlue)
                     )
         ) {
-            Row {
+            Row {val gambar = painterResource(id = R.drawable.umy_logo)
             }
         }
     }
