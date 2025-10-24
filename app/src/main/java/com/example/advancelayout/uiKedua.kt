c2e57643a19505b3aec0470973e6135ce8697e26
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-24 14:39:44 +0700

    Menambahkan teks alamat2 dengan ukuran lebih kecil

--- app/src/main/java/com/example/advancelayout/uiKedua.kt
+++ app/src/main/java/com/example/advancelayout/uiKedua.kt
@@ -68,6 +68,12 @@ fun AktivitasKedua(modifier: Modifier) {
                         color = Color.White,
                         modifier = Modifier.padding(top = 15.dp)
                     )
+                    Text(
+                        text = stringResource(id = R.string.alamat2),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
                 }
             }
         }
